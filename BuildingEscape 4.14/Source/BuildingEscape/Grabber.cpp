3aab7997730d0b7f7b743528534dbf7bb283d77c
Proprietario-PC\Proprietario <acampora199@gmail.com>
2017-02-23 14:19:15 +0000

    created a funtcion to see what object is the character facing

--- BuildingEscape 4.14/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape 4.14/Source/BuildingEscape/Grabber.cpp
@@ -38,15 +38,46 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 		OUT PlayerViewPointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("location: %s, Position: %s"),
+	/*UE_LOG(LogTemp, Warning, TEXT("location: %s, Position: %s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
-	);
+	);*/
 
 	FVector LineTraceDirection = PlayerViewPointRotation.Vector();
 	FVector LineTraceEnd = PlayerViewPointLocation + LineTraceDirection * Reach;
 	// Ray-Cast out to reach distance
-	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.f);
-	//See what we hit
+	DrawDebugLine(
+		GetWorld(), 
+		PlayerViewPointLocation, 
+		LineTraceEnd, 
+		FColor(255, 0, 0), 
+		false, 
+		0.f, 
+		0.f, 
+		10.f
+	);
+	///Setup query parameters
+
+	FCollisionQueryParams TracePar(FName(TEXT("")), false, GetOwner());
+
+	///Line-trace (aka ray-cast) out of reach distance
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TracePar
+	);
+	///See what we hit
+	
+	AActor* ActorHit = Hit.GetActor();
+	
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Line trace it: %s"), *(ActorHit->GetName()))
+	}
+
 }
 
