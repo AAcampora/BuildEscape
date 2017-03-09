19b160cd187ae43fca645f293f7af05f7182acdb
Proprietario-PC\Proprietario <acampora199@gmail.com>
2017-03-09 17:08:50 +0000

    added a deegate to blueprint

--- BuildingEscape 4.14/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape 4.14/Source/BuildingEscape/OpenDoor.h
@@ -5,6 +5,7 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -25,7 +26,10 @@ public:
 	void OpenDoor();
 	void CloseDoor();
 	
-		
+	UPROPERTY(BlueprintAssignable)
+
+		FOnOpenRequest OnOpenRequest;
+
 private:
 	UPROPERTY(EditAnywhere)
 	float OpenAngle = 60.0f;
